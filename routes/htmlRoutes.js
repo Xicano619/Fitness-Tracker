37e7c7b0b1d37161d7852292b3c59a1c97cdab7a
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-02-06 10:07:30 -0800

    Required router = require(express).Router().

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,15 @@
 path = require("path");
+const router = require("express").Router();
 
-module. exports = (app => {
-    app.get("/stats", (req, res) => {
-        res.sendFile(path.join(_dirname, "../public/stats.html"));
-    });
-
-    app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(_dirname, "../public/exercise.html"));
-    });
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+module.exports = router;
