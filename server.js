71863ab8a980cac30e412eb6c337d5fcb9717b95
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-02-09 21:39:47 -0800

    Update mongodb connection.

--- server.js
+++ server.js
@@ -14,13 +14,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workoutdb", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
-
 // routes
 app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
